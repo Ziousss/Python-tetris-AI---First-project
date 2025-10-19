ba7b5dabbf52ff6eed25c0b18af3f02bdacc0543
Ziousss <johnsson_daniel@outlook.com>
2025-10-19 22:50:21 +0100

    Added the fact that it can see 2 pieces ahead

--- DNQAI.py
+++ DNQAI.py
@@ -37,111 +37,143 @@ PIECES = {
 }
 
 generation = 200
-individuals = 500
+individuals = 400
 mutation = 0.15
 mutation_step = 0.2
 
 population = []
+
 for _ in range(individuals):  
     weights = {
-        "lines": random.uniform(-2, 2),
-        "holes": random.uniform(-2, 2),
-        "height": random.uniform(-2, 2),
-        "bumpiness": random.uniform(-2, 2)
+        "lines": random.uniform(0.5, 2),
+        "holes": random.uniform(0, 2),
+        "height": random.uniform(0, 2),
+        "bumpiness": random.uniform(0, 2)
     }
     population.append({"weights": weights, "fitness": 0})
 
 
 for i in range(generation):
-
     # Looping through every bot
     for j in range(individuals):
         weights = population[j]["weights"]
+
         current_piece_type = random.choice(Pieces_list)
         current_piece = Functions(PIECES[current_piece_type], x=4, y=0)
         next_piece_type = random.choice(Pieces_list)
         next_piece = Functions(PIECES[next_piece_type], x=4, y=0)
+        second_next_piece_type = random.choice(Pieces_list)
+        second_next_piece = Functions(PIECES[next_piece_type], x=4, y=0)
 
         board = [[0 for _ in range(10)] for _ in range(20)]
 
         back_to_back = False
         total_score = 0
+        human_score = 0
         count = 0
 
         # Game itself
         while True:
-            # Calculates all possibilities
-            if current_piece_type == 'O':
-                max_rotations = 1
-            elif current_piece_type == 'I':
-                max_rotations = 2
-            else:
-                max_rotations = 4
-
+            reward = 0
+            score = 0
+            count = 0
             best_score = -float('inf')
             best_rotation = None
             best_position = None
 
-            for rotations in range(max_rotations):
-                temp_piece = Functions(PIECES[current_piece_type], x=4, y=0)
+            if current_piece_type == "O":
+                max_rotation = 1
+            elif current_piece_type == "I":
+                max_rotation = 2
+            else:
+                max_rotation = 4
 
-                for _ in range(rotations):
-                    temp_piece.rotate_right()
+            for rotation in range(max_rotation):
+                temp_piece_1 = Functions(PIECES[current_piece_type],x=4,y=0)
+                
+                for _ in range(rotation):
+                    temp_piece_1.rotate_right()
 
-                # Loop through columns and drop piece
-                for column in range(0, 10 - len(temp_piece.shape[0]) + 1):
-                    temp_board = [row[:] for row in board]
-                    temp_piece.x = column
-                    temp_piece.y = 0
+                for column in range(0, 10 - len(temp_piece_1.shape[0]) + 1):
+                    temp_board_1 = [row[:] for row in board]
+                    temp_piece_1.x = column
+                    temp_piece_1.y = 0
 
-                    while not Collision.collision_piece_bottom(temp_piece, temp_board):
-                        piece_height = len(temp_piece.shape)
-                        if temp_piece.y + piece_height >= 20:
-                            break
-                        temp_piece.y += 1
+                    while not Collision.collision_piece_bottom(temp_piece_1,temp_board_1):
+                        temp_piece_1.y += 1 
 
-                    # Lock piece & compute reward
-                    temp_board = Functions.lockBoard(temp_piece, temp_board)
-                    temp_board, lines_cleared = Functions.clear_lines(temp_board)
-                    state = Functions.make_state(temp_board, next_piece_type)
-                    score, back_to_back_temp = Functions.score_count(lines_cleared, back_to_back)
-
-                    reward = Functions.compute_reward_geneticAI(temp_piece, temp_board, lines_cleared, state, weights) + score
-
-                    if reward > best_score:
-                        best_score = reward
-                        best_rotation = rotations
-                        best_position = column
+                    temp_board_1 = Functions.lockBoard(temp_piece_1, temp_board_1)
+                    temp_board_1, lines_cleared_1 = Functions.clear_lines(temp_board_1)
+                    score_1, back_to_back_temp = Functions.score_count(lines_cleared_1,back_to_back)
             
-            # Applies the best one
+                    if next_piece_type == "O":
+                        next_max_rotation = 1
+                    elif next_piece_type == "I":
+                        next_max_rotation = 2
+                    else:
+                        next_max_rotation = 4
+
+                    for next_rotation in range(next_max_rotation):
+                        temp_piece_2 = Functions(PIECES[next_piece_type],x=4,y=0)
+                        
+                        for _ in range(next_rotation):
+                            temp_piece_2.rotate_right()
+                        for next_column in range(0, 10 - len(temp_piece_2.shape[0]) + 1):
+                            temp_board_2 = [row[:] for row in temp_board_1]
+                            temp_piece_2.x = next_column
+                            temp_piece_2.y = 0
+
+                            while not Collision.collision_piece_bottom(temp_piece_2,temp_board_2):
+                                temp_piece_2.y += 1 
+
+                            temp_board_2 = Functions.lockBoard(temp_piece_2,temp_board_2)
+                            temp_board_2, lines_cleared_2 = Functions.clear_lines(temp_board_2)
+                            score_2, _ = Functions.score_count(lines_cleared_2,back_to_back_temp)
+
+                            total_lines = lines_cleared_2 + lines_cleared_1
+
+                            state = Functions.make_state(temp_board_2, second_next_piece_type)
+                            reward = Functions.compute_reward(temp_piece_2,temp_board_2,total_lines,state)
+
+                            if reward > best_score:
+                                best_score = reward
+                                best_rotation = rotation
+                                best_position = column
+            
+            #Applies the best one
             for _ in range(best_rotation):
                 current_piece.rotate_right()
             current_piece.x = best_position
 
-            while not Collision.collision_piece_bottom(current_piece, board):
+            while not Collision.collision_piece_bottom(current_piece,board):
                 current_piece.y += 1
-
-            board = Functions.lockBoard(current_piece, board)
-            board, lines_cleared = Functions.clear_lines(board)
-
-            score, back_to_back = Functions.score_count(lines_cleared, back_to_back)
-            count += 1  #Values long game
-            total_score += score + count * 0.05 #Clear lines instead of just staying alive
+                count += 1
+                
+            board = Functions.lockBoard(current_piece,board)
+            board,line_cleard_def = Functions.clear_lines(board)
 
             current_piece = next_piece
             current_piece_type = next_piece_type
-            next_piece_type = random.choice(Pieces_list)
-            next_piece = Functions(PIECES[next_piece_type], x=4, y=0)
-            
-            if Functions.endgame(current_piece, board):
+            next_piece_type = second_next_piece_type
+            next_piece = second_next_piece
+            second_next_piece_type = random.choice(Pieces_list)
+            second_next_piece = Functions(PIECES[second_next_piece_type],x=4,y=0)
+
+
+            score,back_to_back = Functions.score_count(line_cleard_def,back_to_back)
+            total_score += score 
+            human_score += score
+            human_score += count*2
+
+            print(f"{human_score}")
+
+            if Functions.endgame(current_piece,board):
                 break
 
-        print(f"Bot {j} gen {i} finished with score: {total_score}")
         population[j]["fitness"] = total_score
 
-        if j%10 == 0:
-            with open("genetic_Bot_AI.txt", "a") as f:
-                f.write(f"Generation {i} Bot {j}, score = {total_score}\n")
+        with open(f"human_score.txt","a") as f:
+            f.write(f"{j} has human score {human_score}")
 
     # Creates the new generation
     population = sorted(population, key=lambda x: x["fitness"], reverse=True)
@@ -150,14 +182,13 @@ for i in range(generation):
     avg_fit = sum(p["fitness"] for p in population) / len(population)
 
     with open("genetic_generation_AI.txt", "a") as f:
-        f.write(f"Generation {i}: Best = {best_fit:.2f}, best weight = {population[0]["weights"]}, Avg = {avg_fit:.2f}\n")
+        f.write(f"Generation {i}: Best = {best_fit:.2f}, best weight = {population[0]["weights"]}, best fitness = {population[0]["fitness"]}, Avg = {avg_fit:.2f}\n")
 
-    variable = individuals // 5
-    new_pop = population[:variable]
-    i1 = random.randrange(0, variable)
-    i2 = random.randrange(0, variable)
-    
-    for _ in range(individuals - variable):
+    new_pop = population[individuals*0.1]
+
+    for _ in range(individuals*0.75):
+        i1 = random.randrange(0, individuals*0.75)
+        i2 = random.randrange(0, individuals*0.75)
         if random.random()<0.5:
             weights = {
                 "lines": (new_pop[i1]["weights"]["lines"] + new_pop[i2]["weights"]["lines"]) / 2,
@@ -166,16 +197,29 @@ for i in range(generation):
                 "bumpiness": (new_pop[i1]["weights"]["bumpiness"] + new_pop[i2]["weights"]["bumpiness"]) / 2
             }
         else:
-            weights = random.choice([new_pop[i1],new_pop[i2]])
+            weights = random.choice([new_pop[i1]["weights"], new_pop[i2]["weights"]]).copy()
 
         for key in weights:
-            if random.random() < mutation:  
+            if random.random() < mutation:
                 weights[key] += random.uniform(-mutation_step, mutation_step)
+        
+        weights["lines"] = max(weights["lines"],0)
 
         new_pop.append({"weights": weights, "fitness": 0})
     
+    for _ in range(individuals*0.15):  
+        weights = {
+            "lines": random.uniform(0.5, 2),
+            "holes": random.uniform(0, 2),
+            "height": random.uniform(0, 2),
+            "bumpiness": random.uniform(0, 2)
+        }
+        population.append({"weights": weights, "fitness": 0})
+
+    
     population = new_pop
-    mutation = max(mutation*0.998,0.05)
+
+    mutation = max(mutation*0.995,0.05)
     mutation_step = max(mutation_step*0.998,0.1)
 
 print("Training complete!")
